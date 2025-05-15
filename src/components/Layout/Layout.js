a97a604f09ac5857999d66c34b6ed0643188b655
TheMrBooblik <hobby@pingwi.com>
2025-05-15 16:29:07 +0200

    feat: head updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -18,7 +18,10 @@ const Layout = ({children}) => {
   return (
     <div className={styles.layout}>
       <Head>
-        <link rel="icon" href="/favicon.ico"/>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚂</text></svg>"
+        />
       </Head>
       <Header setModalIsOpen={setModalIsOpen} setLayerType={setLayerType} layerType={layerType}/>
       <main className={styles.main}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,11 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Pingwi Sleeper Train Map</title>
-        <meta name="description" content="Pingwi Sleeper Train Ma"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta name="description" content="The Sleeper Train Map is a web application that visualizes direct night sleeper train routes across Europe. By selecting a station, users can easily see all reachable destinations via sleeper trains."/>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚂</text></svg>"
+        />
       </Head>
     </Layout>
   )
